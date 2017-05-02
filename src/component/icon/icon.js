279a12774e615f6f368a4b50aa2f21f62a82335e
fedesigner <10543563@qq.com>
2017-05-02 16:28:36 +0800

    add 7 icons

--- src/component/icon/icon.js
+++ src/component/icon/icon.js
@@ -1,13 +1,27 @@
 import iBack from './svg/back.svg';
+import iClock from './svg/clock.svg';
+import iClose from './svg/close.svg';
+import iEdit from './svg/edit.svg';
 import iHome from './svg/home.svg';
+import iInfo from './svg/info.svg';
 import iMore from './svg/more.svg';
+import iPlus from './svg/plus.svg';
 import iTick from './svg/tick.svg';
+import iTrash from './svg/trash.svg';
 import iUser from './svg/user.svg';
+import iVolume from './svg/volume.svg';
 
 export default {
     'back': iBack,
+    'clock': iClock,
+    'close': iClose,
+    'edit': iEdit,
     'home': iHome,
+    'info': iInfo,
     'more': iMore,
+    'plus': iPlus,
     'tick': iTick,
-    'user': iUser
+    'trash': iTrash,
+    'user': iUser,
+    'volume': iVolume
 }
--- test/component/other.vue
+++ test/component/other.vue
@@ -1,22 +1,74 @@
 <style>
-    .icon .fm-list-item--wrapper {
-        justify-content: space-around;
+    .icon .fm-list-item--wrapper > div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 25%;
+        line-height: 2;
+        font-size: 3.333vw;
     }
 </style>
 
 <template>
     <div id="wrapper">
         <fm-header @back="back" :statusbar="false" title="Flyme UI - 其他"></fm-header>
-        <fm-list :underline="false" title="Icon 图标" class="icon">
+        <fm-list :underline="false" :active-effect="false" title="Icon 图标" class="icon">
             <fm-list-item>
-                <fm-icon id="back"></fm-icon>
-                <fm-icon id="home"></fm-icon>
-                <fm-icon id="more"></fm-icon>
-                <fm-icon id="tick"></fm-icon>
-                <fm-icon id="user"></fm-icon>
+                <div>
+                    <fm-icon id="back"></fm-icon>
+                    <div>back</div>
+                </div>
+                <div>
+                    <fm-icon id="clock"></fm-icon>
+                    <div>clock</div>
+                </div>
+                <div>
+                    <fm-icon id="close"></fm-icon>
+                    <div>close</div>
+                </div>
+                <div>
+                    <fm-icon id="edit"></fm-icon>
+                    <div>edit</div>
+                </div>
+            </fm-list-item>
+            <fm-list-item>
+                <div>
+                    <fm-icon id="home"></fm-icon>
+                    <div>home</div>
+                </div>
+                <div>
+                    <fm-icon id="info"></fm-icon>
+                    <div>info</div>
+                </div>
+                <div>
+                    <fm-icon id="more"></fm-icon>
+                    <div>more</div>
+                </div>
+                <div>
+                    <fm-icon id="plus"></fm-icon>
+                    <div>plus</div>
+                </div>
+            </fm-list-item>
+            <fm-list-item>
+                <div>
+                    <fm-icon id="tick"></fm-icon>
+                    <div>tick</div>
+                </div>
+                <div>
+                    <fm-icon id="trash"></fm-icon>
+                    <div>trash</div>
+                </div>
+                <div>
+                    <fm-icon id="user"></fm-icon>
+                    <div>user</div>
+                </div>
+                <div>
+                    <fm-icon id="volume"></fm-icon>
+                    <div>volume</div>
+                </div>
             </fm-list-item>
         </fm-list>
-        <fm-list title="Tag 标签">
+        <fm-list :active-effect="false" title="Tag 标签">
             <fm-list-item>
                 <fm-tag>标签</fm-tag>
                 <fm-tag color="#3bc06b">标签</fm-tag>
